b9b919db6d924046356051baf83b454f36103edc
Aleksandar Tosic <tosha90@gmail.com>
2023-04-13 13:03:51 +0200

    feature: add database models

---
+++ server/database/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Role',
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
