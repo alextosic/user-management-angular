6e690914210e33deed72ba4669e065170c7e8844
Aleksandar Tosic <tosha90@gmail.com>
2023-04-19 17:43:26 +0200

    feature: create the error message display functionality

--- client/src/app/auth/auth-error.interceptor.ts
+++ client/src/app/auth/auth-error.interceptor.ts
@@ -2,20 +2,26 @@ import { Injectable } from '@angular/core';
 import { tap, Observable } from 'rxjs';
 import { HttpHandler, HttpInterceptor, HttpRequest, HttpEvent } from '@angular/common/http';
 
+import { MessageService } from '../message/message.service';
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthErrorInterceptor implements HttpInterceptor {
+  constructor(private messageService: MessageService) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.messageService.hide();
+
     return next.handle(req)
       .pipe(
         tap({
           error: (err) => {
-            if (err.status === 401 || err.status === 403) {
+            if (err.status === 401) {
               console.log('User unauthorized, logging out');
             }
 
-            console.log(`Showing error toast message - ${err.error.message}`);
+            this.messageService.show(err.error.message);
           }
         })
       );
