22e185c67b5acc4407efbccd6221d841b44d997d
Aleksandar Tosic <tosha90@gmail.com>
2023-04-25 14:16:20 +0200

    feature: create role controller, routes and validators

--- server/router/index.js
+++ server/router/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const authRoutes = require('./auth');
 const profileRoutes = require('./profile');
 const userRoutes = require('./user');
+const roleRoutes = require('./role');
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/profile', profileRoutes);
 router.use('/user', userRoutes);
+router.use('/role', roleRoutes);
 
 module.exports = router;
