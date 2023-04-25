b32aa3b76ea698f665b044c204aaafdd2d8985d2
Aleksandar Tosic <tosha90@gmail.com>
2023-04-25 13:23:19 +0200

    feature: create permissions migrations

--- server/database/migrations/20230418132603-add_user_and_admin_roles.js
+++ server/database/migrations/20230418132603-add_user_and_admin_roles.js
@@ -2,8 +2,14 @@ const { defaultRoles } = require('../../constants/role');
 
 module.exports = {
   async up(db, client) {
+    const permissions = await db.collection('permissions').find({}).toArray();
+
     await Promise.all(Object.values(defaultRoles).map(async (roleName) => {
-      await db.collection('roles').insertOne({ name: roleName });
+      const rolePermissions = roleName === defaultRoles.ADMIN
+        ? permissions.map((permission) => permission._id)
+        : [];
+
+      await db.collection('roles').insertOne({ name: roleName, permissions: rolePermissions });
     }));
   },
 
