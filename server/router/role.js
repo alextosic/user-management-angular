7cda11bd2bf9e3c412c6be83127132a726b2871a
Aleksandar Tosic <tosha90@gmail.com>
2023-04-25 14:32:37 +0200

    feature: update authorization middleware to work with permissions instead of roles

--- server/router/role.js
+++ server/router/role.js
@@ -4,14 +4,48 @@ const roleController = require('../controllers/role');
 const authMiddleware = require('../middleware/auth');
 const roleValidator = require('../middleware/validators/role');
 
-const { defaultRoles } = require('../constants/role');
+const { defaultPermissions } = require('../constants/permission');
 
 const router = express.Router();
 
-router.get('/all', authMiddleware.authenticate(), authMiddleware.authorize([defaultRoles.ADMIN]), roleValidator.validateGetAllRoles(), roleController.getAllRoles());
-router.get('/:id', authMiddleware.authenticate(), authMiddleware.authorize([defaultRoles.ADMIN]), roleValidator.validateGetRole(), roleController.getRole());
-router.post('/', authMiddleware.authenticate(), authMiddleware.authorize([defaultRoles.ADMIN]), roleValidator.validateCreateRole(), roleController.createRole());
-router.patch('/:id', authMiddleware.authenticate(), authMiddleware.authorize([defaultRoles.ADMIN]), roleValidator.validateUpdateRole(), roleController.updateRole());
-router.delete('/:id', authMiddleware.authenticate(), authMiddleware.authorize([defaultRoles.ADMIN]), roleValidator.validateDeleteRole(), roleController.deleteRole());
+router.get(
+  '/all',
+  authMiddleware.authenticate(),
+  authMiddleware.authorize([defaultPermissions.listRoles]),
+  roleValidator.validateGetAllRoles(),
+  roleController.getAllRoles(),
+);
+
+router.get(
+  '/:id',
+  authMiddleware.authenticate(),
+  authMiddleware.authorize([defaultPermissions.viewRoles]),
+  roleValidator.validateGetRole(),
+  roleController.getRole(),
+);
+
+router.post(
+  '/',
+  authMiddleware.authenticate(),
+  authMiddleware.authorize([defaultPermissions.createRoles]),
+  roleValidator.validateCreateRole(),
+  roleController.createRole(),
+);
+
+router.patch(
+  '/:id',
+  authMiddleware.authenticate(),
+  authMiddleware.authorize([defaultPermissions.updateRoles]),
+  roleValidator.validateUpdateRole(),
+  roleController.updateRole(),
+);
+
+router.delete(
+  '/:id',
+  authMiddleware.authenticate(),
+  authMiddleware.authorize([defaultPermissions.deleteRoles]),
+  roleValidator.validateDeleteRole(),
+  roleController.deleteRole(),
+);
 
 module.exports = router;
